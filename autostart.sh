26ab4a24cb955c089ddd296b1ede7447bbab3622
Ken McKenzie <slain.12@gmail.com>
2023-01-21 22:22:35 +0200

    Initial Payload, basic Setup

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#Startup Apps
+
+picom -b &
+feh --bg-scale ~/.config/background.jpg &
+#/usr/lib/polkit-1/polkit-agent-helper-1 &
+#/usr/lib/polkit-1/polkitd &
+/usr/bin/lxpolkit &
+pasystray &
